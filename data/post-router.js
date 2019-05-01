f96d2a5c74116725bd23d25e7756aa5ae02f8e7c
wei du <weidu@weis-Mac-mini.hsd1.va.comcast.net>
2019-04-30 21:09:17 -0400

    put worked properly

--- data/post-router.js
+++ data/post-router.js
@@ -50,13 +50,13 @@ router.delete('/:id', async (req, res) => {
 router.put('/:id', async (req, res) => {
     try{
         const post = await Posts.update(req.params.id, req.body);
-        // const {title, contents } =req.body;
-        if(post){
+        const {title, contents } =req.body;
+        if(post.id&&title&&contents){
             res.status(200).json(post);
         }
-        // else if(title&&contents){
-        //     res.status(400).json({ message: "The post with the specified ID does not exist." })
-        // }
+        else if(title&&contents){
+            res.status(400).json({ message: "The post with the specified ID does not exist." })
+        }
         else{
             res.status(404).json({ errorMessage: "Please provide title and contents for the post." })
         }} catch (err){ res.status(500).json({ error: "The post information could not be modified." })}
